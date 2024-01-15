4def08b49c45d1e56def55e05a7b492b9593c76c
Aftab Ahmed <aftab.ahmed@astera.com>
2024-01-15 13:39:52 +0500

    Code Cleanup and remove unnecessary files

--- Parsing_WSDL/Program.cs
+++ Parsing_WSDL/Program.cs
@@ -4,8 +4,6 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        //string pathToSave = @"C:\Users\aftab.ahmed\source\repos\Parsing_WSDL\Parsing_WSDL\Example_Wsdl_to_parse\Unit Test\";
-
         string[] filePaths =
         {
             @"C:\Users\aftab.ahmed\source\repos\Parsing_WSDL\Parsing_WSDL\Example_Wsdl_to_parse\wsdl_temp_conversion.wsdl",
@@ -15,24 +13,23 @@ internal class Program
             @"C:\Users\aftab.ahmed\source\repos\Parsing_WSDL\Parsing_WSDL\Example_Wsdl_to_parse\wsdl_sample.wsdl",
             @"C:\Users\aftab.ahmed\source\repos\Parsing_WSDL\Parsing_WSDL\Example_Wsdl_to_parse\corporatesms.svc.wsdl",
             @"C:\Users\aftab.ahmed\source\repos\Parsing_WSDL\Parsing_WSDL\Example_Wsdl_to_parse\salesforce.enterprise.wsdl"
-    };
-        //string prev = "C:\\Users\\aftab.ahmed\\Downloads\\wsdl_temp_conversion.wsdl"; 
-        //string prev = @"C:\Users\aftab.ahmed\source\repos\Parsing_WSDL\Parsing_WSDL\Example_Wsdl_to_parse\soap_doclit.wsdl";
-        //string prev = @"C:\Users\aftab.ahmed\source\repos\Parsing_WSDL\Parsing_WSDL\Example_Wsdl_to_parse\global_weather_wsdl.wsdl";
-        //string prev = @"C:\Users\aftab.ahmed\source\repos\Parsing_WSDL\Parsing_WSDL\Example_Wsdl_to_parse\wsdl_sample.wsdl";
-        //string prev = @"C:\Users\aftab.ahmed\source\repos\Parsing_WSDL\Parsing_WSDL\Example_Wsdl_to_parse\corporatesms.svc.wsdl";
-        //string prev = @"C:\Users\aftab.ahmed\source\repos\Parsing_WSDL\Parsing_WSDL\Example_Wsdl_to_parse\salesforce.enterprise.wsdl";
-        //string prev = @"C:\Users\aftab.ahmed\source\repos\Parsing_WSDL\Parsing_WSDL\Example_Wsdl_to_parse\ndfdXMLserver.php.wsdl";
-        foreach (string file in filePaths)
-        {
-            //string pathToSave = file.Replace("Example_Wsdl_to_parse", "TestCases") + ".json";
-            string pathToSave2 = file.Replace("Example_Wsdl_to_parse", "ComparisonJsons") + ".json";
-            var services = new WSDLParser().Parse(file);
-            var json = JsonConvert.SerializeObject(services);
-            //File.WriteAllText(pathToSave, json);
-            File.WriteAllText(pathToSave2, json);
-        }
+        };
 
-        JsonFileTests.TestJsonFileContentEquality();
+        var services = new WSDLParser().Parse(filePaths[0]);
+
+
+        //TESTING BEFORE AND AFTER CODE REFACTORING CASE
+
+        //foreach (string file in filePaths)
+        //{
+        //    //string pathToSave = file.Replace("Example_Wsdl_to_parse", "TestCases") + ".json";
+        //    string pathToSave2 = file.Replace("Example_Wsdl_to_parse", "ComparisonJsons") + ".json";
+        //    var services = new WSDLParser().Parse(file);
+        //    var json = JsonConvert.SerializeObject(services);
+        //    //File.WriteAllText(pathToSave, json);
+        //    File.WriteAllText(pathToSave2, json);
+        //}
+
+        //JsonFileTests.TestJsonFileContentEquality();
     }
 }
